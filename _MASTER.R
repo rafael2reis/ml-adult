4df720ad1cbb9255c689f2565d25e26a26f11de6
Rafael Reis <rafael2reis@gmail.com>
2015-06-19 15:32:34 -0300

    upd fix on CreateArff

--- _MASTER.R
+++ _MASTER.R
@@ -1,7 +1,9 @@
 rm(list = ls())
 
+# Load the Dataset asStrings
 source("dataset.R")
+LoadAdultDataset(asStrings = TRUE)
 
 # Create de .ARFF files to be loaded in Weka
-LoadAdultDataset(asStrings = TRUE)
+source("create_arff.R")
 CreateArff(train = adult, test = adult.test, relation = 'adult')
